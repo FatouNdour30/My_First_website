0d1c0ecfdabe01afbd65e0139d8d74d82c7e48c1
Fatoulahi <ndourfatoulaye@gmail.com>
2024-03-21 09:12:24 -0700

    how

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My_First_website/', // Chemin de déploiement sur GitHub Pages
+
 })
